e4f09fd8d74083db59137ec8f2d9a7ca140e9d18
TamFultz <gorgonster.tam@gmail.com>
2015-09-20 23:25:37 -0400

    Done

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,15 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(greeting)
+
+if greeting == greeting.upcase
+ "NO, NOT SINCE 1938!"
+
+else 
+  "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+
