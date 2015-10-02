fb87ea2336a8c4ff7465da36a9e35eb1286fd506
Anna Rasshivkina <anna.nigma@yahoo.com>
2015-10-02 18:29:38 -0400

    Done

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,11 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(i_said)
+  if i_said != i_said.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else i_said == i_said.upcase
+    "NO, NOT SINCE 1938!"
+  end
+end
