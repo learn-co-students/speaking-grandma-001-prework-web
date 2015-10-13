898eb935eb80f1ad4e354f89a6c940197e547a28
snowmachine <eoghanjoyce@yahoo.com>
2015-10-13 14:30:34 -0400

    did again done

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
-def speak_to_grandma(i_said)
-  if i_said != i_said.upcase
+def speak_to_grandma(grandma_i_said)
+  if grandma_i_said != grandma_i_said.upcase
      "HUH?! SPEAK UP, SONNY!"
-    else i_said == i_said.upcase
+    else grandma_i_said == grandma_i_said.upcase
       "NO, NOT SINCE 1938!"
   end
 end
