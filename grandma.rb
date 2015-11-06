07b2841188e131bf429a682d4a9920bf3f391afa
BigBadBlue <jamesdeanhead@outlook.com>
2015-11-06 16:44:07 -0500

    Grandma test

--- grandma.rb
+++ grandma.rb
@@ -7,4 +7,11 @@
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
-# NO, NOT SINCE 1938!
+def speak_to_grandma(speak)
+  if speak == speak.upcase
+   "NO, NOT SINCE 1938!"
+  else
+   "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
