c423592f717e5374b716c74c50279f4a9d15aa18
john-a-griffin <john.griffin.simontech@gmail.com>
2015-11-11 00:00:29 +0000

    finished grandma lab

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,16 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(talk)
+  if talk === talk.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+puts speak_to_grandma("whats up")
+puts speak_to_grandma("hi")
+puts speak_to_grandma("GRANNY")
+puts speak_to_grandma("GRANDMOTHER")
+  
