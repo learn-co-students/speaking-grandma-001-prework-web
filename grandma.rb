18d6512851738f9067b7653e2faed51fe68dc156
JosueBravo1998 <josue.bravo.simontech.2016@gmail.com>
2015-11-13 17:01:02 +0000

    Finished speaking grandma lab

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+def speak_to_grandma(talk)
+  if talk === talk.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+puts speak_to_grandma("hello")
+puts speak_to_grandma("hi")
+puts speak_to_grandma("WHAT DID YOU EAT TODAY")
+puts speak_to_grandma("WHAT?")
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
