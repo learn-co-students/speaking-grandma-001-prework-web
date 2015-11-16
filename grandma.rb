2b22c2e577c285ab73398960fe6b1c17d79c0579
ashante <ashantejr@gmail.com>
2015-11-16 18:49:54 +0000

    I'm a coding thug.

--- grandma.rb
+++ grandma.rb
@@ -6,16 +6,12 @@
 # NO, NOT SINCE 1938!
 puts "Ask her what's going on"
 
-def speak_to_grandma()
-  command = ()
-  while command != 'BYE'
-      command = gets.chomp
-      if command != command.upcase
-          puts "HUH?! SPEAK UP, DEAR!"
-      else
-          puts "NO, NOT SINCE 1938!"
-      end
-  end
+def speak_to_grandma(command)
+    if command != command.upcase
+      return "HUH?! SPEAK UP, SONNY!"
+    else
+      return "NO, NOT SINCE 1938!"
+    end
 end
 
-puts speak_to_grandma
+puts speak_to_grandma("Whats up biatch!")
