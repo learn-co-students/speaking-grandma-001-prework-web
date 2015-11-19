351f2fd9c5a9c45e9bdca1ebd1a0b21b270950a5
dorothym <dorothy.v.moore@gmail.com>
2015-11-19 12:31:33 -0500

    add speak_to_grandma method to check for caps

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase.upcase == phrase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+#speak_to_grandma("HI GRANDMA")
