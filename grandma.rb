00cba3f42672f0c2b16e866d10dd6dbc37539400
joeyGITHUBpineda <joey.pineda.simontech.2016@gmail.com>
2015-11-20 17:04:42 +0000

    done

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,17 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+
+def speak_to_grandma(talk)
+  if talk === talk.upcase
+  "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+puts speak_to_grandma ("hello")
+puts speak_to_grandma ("hi")
+puts speak_to_grandma ("WHAT DID YOU EAT TODAY")
+puts speak_to_grandma ("WHAT?")
