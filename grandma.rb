3110e82b0179c3250d6c0dba9628e0e465b737fa
febbraiod <donedesign1@gmail.com>
2015-11-20 17:19:23 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(string)
+
+  if string != string.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
+
+end
