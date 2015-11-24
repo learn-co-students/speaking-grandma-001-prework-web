459e0e565055c5d27eee74bc14da3f34a1873db6
kaylagalway <kayla.galway@gmail.com>
2015-11-23 21:13:41 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+def speak_to_grandma(talk)
+  talk = (talk)
+  if talk == talk.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    talk
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
