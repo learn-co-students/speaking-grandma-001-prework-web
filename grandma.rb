c39e2cb3b86af3ccfec2b09b8a454ec7b6fedfb2
TyJuanABrooks <tyjuanabrooks@gmail.com>
2015-12-05 14:45:46 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,35 @@
+def speak_to_grandma(phrase = "Hi Nana, how are you?".upcase)
+  if phrase == phrase.downcase
+    puts "Hi!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
