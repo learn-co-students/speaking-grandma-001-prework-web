35c13bdfdb1a0d4e60677ae5140045684835a2b9
LGOLIK <golikster@gmail.com>
2015-12-28 14:16:42 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,14 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(text)
+  if text == text.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+speak_to_grandma("hi grandma")
+speak_to_grandma("ARE YOU HUNGRY?")
