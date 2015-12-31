7817c39ce14f2cac6807fce9d0938eeb65318d91
motti770 <mottigottlieb@gmail.com>
2015-12-31 21:51:51 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,7 @@
-# Write a speak_to_grandma method.
-def speak_to_grandma(message)
-                
-  if message == "Hi Nana, how are you?".upcase
-        puts "NO, NOT SINCE 1938!"
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
-
 end
-speak_to_grandma("Hi Nana, how are you?")
