c65964701b21e858cf1bd02a41c955287e0c08a3
kunq <Kun_Qian@msn.com>
2016-01-18 10:55:30 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase.match(/\p{Lower}/) == nil
+    return('NO, NOT SINCE 1938!')
+  else
+    return("HUH?! SPEAK UP, SONNY!")
+  end
+end
+
+#speak_to_grandma("Hi Nana, how are you?")
