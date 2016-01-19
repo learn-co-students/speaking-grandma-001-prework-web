6a968f04b604af2f4d2c8a371e66d906e891c61e
ericajayne <ericajayne.walsh@gmail.com>
2016-01-19 00:35:05 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,10 @@
+def speak_to_grandma(input)
+  if input.upcase == input
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!" 
+  end
+end
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
