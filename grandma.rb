915fa14489cd58e6b8c332808904443663f10391
JoyceMatos <j.matosjoyce@gmail.com>
2016-01-23 23:50:22 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
   else phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
