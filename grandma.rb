9e79412a602a53bcaeeaf74824ee903f0e597a82
jd2rogers2 <jd2rogers2@gmail.com>
2016-01-24 08:49:31 -0500

    donzo

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,11 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase="hello grandma")
+if phrase == phrase.upcase
+"NO, NOT SINCE 1938!"
+else
+  "HUH?! SPEAK UP, SONNY!"
+end
+end
