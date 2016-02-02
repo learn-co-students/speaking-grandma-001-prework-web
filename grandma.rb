8a5aa6d1f1430206ccb3c4d03838efcf678040fe
wlschnee <will@willschneekloth.com>
2016-02-01 19:33:19 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(string)
+  if string == string.upcase
+    print "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
+  else
+    print "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
