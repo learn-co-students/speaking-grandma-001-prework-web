a47c6fba41320015289ffa23c9f8ecd9c429d43d
Erica <erica@Ericas-Mac-mini.local>
2016-02-14 22:41:57 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -10,10 +10,13 @@
 # NO, NOT SINCE 1938!
 
 def speak_to_grandma(phrase)
-    if phrase != gets.chomp.upcase
-        puts "NO, NOT SINCE 1938!"
+    if phrase == phrase.upcase
+        return "NO, NOT SINCE 1938!"
     else
-        puts "HUH?! SPEAK UP, SONNY!"
+        return "HUH?! SPEAK UP, SONNY!"
     end
 end
+
+
+
     
