ad63b7d044a9709deb683b851831aa5f649fa171
iashy <iashy.ruby@gmail.com>
2016-02-25 13:54:42 -0800

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
+
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
@@ -8,3 +9,23 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+
+def speak_to_grandma(takes_string_arguement)
+
+    if takes_string_arguement == takes_string_arguement.upcase
+        return "NO, NOT SINCE 1938!"
+    else
+        return "HUH?! SPEAK UP, SONNY!"
+    end
+    
+end
+
+speak_to_grandma('Hi Nana, how are you?')
+
+
+#This one was super hard for me, but enventually got it! YAY! 😻
+
+
+
+
