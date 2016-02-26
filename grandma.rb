d10ed1004990614bbf30f42377a5b8deef669648
SusanZheng15 <zheng.susan12@gmail.com>
2016-02-26 18:09:59 +0000

    nitrous-backup at Fri Feb 26 18:09:59 UTC 2016

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,19 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
+def speak_to_grandma(phrase = "Hi Nana, how are you?")
+  if phrase == phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+    else if phrase("Hi!") == phrase("Hi!").upcase
+    return "HUH?! SPEAK UP, SONNY!"
+    else if phrase("what did you eat today?").upcase
+    return "NO, NOT SINCE 1938!"
+    else if phrase("what?").upcase
+    return "NO, NOT SINCE 1938!"
+    else
+      return "NO, NOT SINCE 1938!"
+  end
