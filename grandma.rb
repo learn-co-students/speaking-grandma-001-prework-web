05fb879f547f2b433fb43a8baa8019d5391b57b6
joshgreen1227 <joshgreen1227@gmail.com>
2016-03-05 09:50:23 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,12 @@
+def speak_to_grandma(stringy)
+  if stringy == stringy.upcase
+    "NO, NOT SINCE 1938!"
+  else "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
