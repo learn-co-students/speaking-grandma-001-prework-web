3d730ecf554d549a2e3b7636af3b4d0e13748ac5
jmkiley <jordankiley@gmail.com>
2016-03-09 15:14:33 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,13 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+def speak_to_grandma(saying)
+  if saying != saying.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
