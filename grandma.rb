fd09cc59593c69583b6fb9c78036ee32ce63e38d
eaharonoff <emanuel.aharonoff@gmail.com>
2016-04-03 22:43:32 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,14 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(a_phrase)
+  if a_phrase !=a_phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "NO, NOT SINCE 1938!"
+  end
+end
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
