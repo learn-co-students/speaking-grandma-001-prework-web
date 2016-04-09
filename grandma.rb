7f9fb267c0cccdff184d22d7dddc91f5d1656d0b
areyes516 <areyes516@yahoo.com>
2016-04-09 02:21:17 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,20 +1,9 @@
-def speak_to_grandma(phrase="Hi Nana, how are you?")
-
-  puts "#{phrase}"
-a="Hi Nana, how are you?".upcase
-b="Hi Nana, how are you?"
-  c= "what did you eat today?".upcase
-  d="what did you eat today?"
-  if a=b
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-    else
-   return phrase
-    if c==d
+  else
     return "NO, NOT SINCE 1938!"
-    else
-    return phrase
-end
-end
+  end
 end
 
 
