c05b4179dd5134b1d139c3bbbfabed07a9678baa
Arsalan-A <zarsalan18@yahoo.com>
2016-04-09 00:49:26 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,20 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+
+def speak_to_grandma(shout)
+  if shout == shout.upcase
+
+    "NO, NOT SINCE 1938!"
+
+  else 
+      
+     "HUH?! SPEAK UP, SONNY!"
+
+  end
+
+end
+
+speak_to_grandma("hello")
+
