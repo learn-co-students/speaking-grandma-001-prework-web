0f49c5e8f5ac4e23178300d6268528c20c785f4b
jackfeely <jack.feely@gmail.com>
2016-04-13 17:46:16 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  end
+end
+
+speak_to_grandma("Hi Nana, how are you?")
