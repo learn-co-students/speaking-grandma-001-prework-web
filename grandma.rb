e5080d6765dcc6950e34450a303d24bf7d1c893c
quarthex15 <andrew.klein.j@gmail.com>
2016-04-17 21:11:42 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,10 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+def speak_to_grandma(phrase)
+  if(phrase == phrase.upcase)
+    answer = "NO, NOT SINCE 1938!"
+  else
+    answer = "HUH?! SPEAK UP, SONNY!"
+  end
+end
