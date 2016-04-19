23922636eb7eb8be2be0362f4ea68b46f1bfb68b
wvazquez <vazquezwilmer@gmail.com>
2016-04-19 16:18:04 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+  phrase != phrase.upcase ? "HUH?! SPEAK UP, SONNY!" : "NO, NOT SINCE 1938!"
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
