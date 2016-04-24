5d66e9215d3d38f30fec22893cdd98a903d6eed2
Ahmet Calis <calisahmet@gmail.com>
2016-04-23 21:09:09 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
    
-  if string.upcase == string
+  if string == string.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
