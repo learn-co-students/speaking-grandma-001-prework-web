935339a721ebf26f4ccb43ad485700c0ee61bd9d
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-10 13:55:10 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,22 +1,7 @@
-def speak_to_grandma(words)
-
-  if words == words.upcase
-
-    puts "NO, NOT SINCE 1938!"
-    speak_to_grandma = "NO, NOT SINCE 1938!"
-    speak_to_grandma
-    else
-      puts "HUH?! SPEAK UP, SONNY!"
-      speak_to_grandma = "HUH?! SPEAK UP, SONNY!"
-      speak_to_grandma
-    end
-
+def speak_to_grandma(arg)
+  if arg == arg.upcase
+    return 'NO, NOT SINCE 1938!'
+  else
+    return 'HUH?! SPEAK UP, SONNY!'
+  end
 end
-
-speak_to_grandma('Hi Nana, how are you?')
-
-speak_to_grandma('Hi!')
-
-speak_to_grandma('WHAT DID YOU EAT TODAY?')
-
-speak_to_grandma('WHAT?')
