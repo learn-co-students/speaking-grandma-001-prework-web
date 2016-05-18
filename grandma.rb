a3d5f34411491feb6cc8fd6a3154d82e882f25b7
Ekaterina Petushova <jevsador@yandex.ru>
2016-05-17 22:43:47 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,18 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
+def speak_to_grandma(phrase)
+   if phrase == phrase.upcase
+     "NO, NOT SINCE 1938!"
+   else
+     "HUH?! SPEAK UP, SONNY!"
+   end
+ end
+ puts speak_to_grandma("whats up")
+ puts speak_to_grandma("hi")
+ puts speak_to_grandma("WHAT DID YOU EAT TODAY")
+ puts speak_to_grandma("WHAT?")
