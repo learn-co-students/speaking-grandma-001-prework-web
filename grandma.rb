98fe6c8c8d65d8bcea820797f00ca1e6b99c2b51
domo1664 <domo1664@gmail.com>
2016-05-22 21:34:51 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,11 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    shouting = "NO, NOT SINCE 1938!"
+  else 
+    cant_hear = "HUH?! SPEAK UP, SONNY!"
+  end
+end
