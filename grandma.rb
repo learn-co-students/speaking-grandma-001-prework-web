d30a5bc6f079faf69b1624c8bad3f643a2681c98
avisee <aseeram23@gmail.com>
2016-05-29 09:56:06 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,16 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase="Hi Nana, how are you?")
+  if phrase == "Hi Nana, how are you?"
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    "NO, NOT SINCE 1938!"
+  else 
+    phrase == "WHAT?"
+      "NO, NOT SINCE 1938!"
+  end
+end
