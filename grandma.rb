33ac16609eb04e3824c798d8d04cd43f5a60ef87
Annabelf11 <fergusonannabel@gmail.com>
2016-05-31 17:58:23 -0400

    Completed Method

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,16 @@
-# Write a speak_to_grandma method.
+
+def speak_to_grandma(phrase)
+  if phrase == "What did you eat today?".upcase
+  "NO, NOT SINCE 1938!"
+elsif phrase == "what?".upcase
+  "NO, NOT SINCE 1938!"
+  else phrase == "Hi Nana, how are you?".downcase
+   "HUH?! SPEAK UP, SONNY!"
+  end 
+end
+
+
+  # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
