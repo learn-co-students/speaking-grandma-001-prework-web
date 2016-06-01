1fab822e5cfac0b87d89e66972e0690020b09343
Salome Braganza <salomekbg@gmail.com>
2016-06-01 10:16:25 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,9 @@
 # NO, NOT SINCE 1938!
 
 def speak_to_grandma(string)
-  if string == string.upcase
+  if string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
