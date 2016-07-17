246d11694a55fa85a11268450d3ba6d21dca3520
Syed Mahmood <ssu.mahmood@gmail.com>
2016-07-17 22:44:23 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,14 +13,14 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma (phrase)
-  if phrase=="Hi Nana, how are you?" || phrase=="Hi!"
-    "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase=="I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase=="WHAT DID YOU EAT TODAY?" || phrase=="WHAT?"
-    "NO, NOT SINCE 1938!"
 
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 #speak_to_grandma("WHAT")
