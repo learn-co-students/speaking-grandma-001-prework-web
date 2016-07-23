a1a0c3ff57207dbed32b238397a4629e80337385
Cparusso <cparusso@gmail.com>
2016-07-23 14:26:13 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,16 @@
+def speak_to_grandma(words)
+  puts "Turd"
+if words == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+elsif words == words.upcase
+  "NO, NOT SINCE 1938!"
+else 
+  "HUH?! SPEAK UP, SONNY!"
+end
+end
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
