54ed4282856df109da68aa446407d7cd92bb6810
hkim53 <paragon.hankido@gmail.com>
2016-07-27 13:10:55 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
-def speak_to_grandma(phrase)
 
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else 
-    return "NO, NOT SINCE 1938!"  
-  end 
 
+
+
+def speak_to_grandma(phrase) 
+  if phrase != phrase.upcase() 
+  return "HUH?! SPEAK UP, SONNY!" 
+elsif phrase=="I LOVE YOU GRANDMA!" 
+  "I LOVE YOU TOO PUMPKIN!" 
+else 
+  return "NO, NOT SINCE 1938!" 
+end 
 end
-
-speak_to_grandma("HI GRANDMA")
