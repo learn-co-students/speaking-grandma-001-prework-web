d69ae3d873e52573897062135d27bf0c2df430c9
Michael Wiggins <michael.wiggins@me.com>
2016-08-13 16:25:05 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,13 @@
+#How do you read and interpret instructions? That's the game here.
+#The hard part of this lesson was interpreting the instructions logic.
+#The keywords are "Whatever" "Unless" "However, if"
+#The "However, if" condition has to be addressed first, because computers are stupid.
+#The program has to understand how and when to address each possibility. It's got to know your priorities.
 # Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# Whatever you say to grandma, she should respond with HUH?! SPEAK UP, SONNY!
+# Unless you shout it (type in all capitals).
+# If you shout, she can hear you (or at least she thinks so) and yells back NO, NOT SINCE 1938!
+# However if you say 'I LOVE YOU GRANDMA!', she should respond with 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
